dcda452066deeb438b720c38749b363b6da61226
Venna Nagarjuna Reddy <nag@Vennas-MacBook-Pro.local>
2024-02-17 14:54:32 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //System.setProperty("webdriver.chrome.driver","");
+        //WebDriver driver=new FirefoxDriver();
+        //driver.get("https://www.google.com/");
+    }
+}
